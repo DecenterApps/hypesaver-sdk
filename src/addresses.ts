01b0193791d28edc84efc587c07ae5f51c5e855e
Aleksa Uroseivc <aleksa.urosevic0@gmail.com>
2025-06-24 14:31:01 +0200

    chore: Update address config

--- src/addresses.ts
+++ src/addresses.ts
@@ -5,7 +5,8 @@ export const actionAddresses = {
 
   PullToken: '0x077d1831b6d7ad53e1393dd0bd79d34b4c045918',
   SendToken: '0x99ef1612dbd3b3e4fd332885f925a5f9123446b5',
-  LSVSell: '0x730021c635b92af537e23727baeaac44b488b519',
+  DFSSell: '0x33aff4acee8c0c1d1ec94eefa3f061a1f426e451',
+  LSVSell: '0x33aff4acee8c0c1d1ec94eefa3f061a1f426e451',
 
   AaveV3Borrow: '0x9b25a772deb74808ec0e1c4ca97f28cf975aebe5',
   AaveV3Supply: '0xf21f51c3c3a67e2bf980ddf08d80fbb1d49634f3',
@@ -15,6 +16,8 @@ export const actionAddresses = {
 
   LSVSupply: '0xa489d6972c9d2db3afd278ac3ff69acf55b881f4',
   LSVBorrow: '0xe2574a2e61aa083d4685e49f5d494589e729bd4c',
+  LSVPayback: '0x6768868d77fa7147083f5849625fb203ac4e37ed',
+  LSVWithdraw: '0xc6eb6091b4e07cbb64badbb3dc6a0e6b6c5e2357',
 
   RecipeExecutor: '0x639cd0dc7c511392eabc5265baacb8df2a0039d1',
 
